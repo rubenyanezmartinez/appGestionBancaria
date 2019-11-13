c1a38e26c04657eedaad43ef4798db438b7ebb11
Ruben <rymartinez@esei.uvigo.es>
2019-11-13 12:01:22 +0100

    Gestión de clientes
    Se completan la clase del dominio de Cliente y su repositorio para añadir la lógica necesaria.

--- App Gestion Bancaria/Core/Acceso a datos/ClientesRepositorio.cs
+++ App Gestion Bancaria/Core/Acceso a datos/ClientesRepositorio.cs
@@ -1,12 +1,106 @@
-﻿using System;
+﻿using App_Gestion_Bancaria.Core.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace App_Gestion_Bancaria.Core.Acceso_a_datos
 {
-    public class ClientesRepositorio
+    public static class ClientesRepositorio
     {
+        public static List<Cliente> RecuperarClientes(string etiquetFichero)
+        {
+            List<Cliente> toret = new List<Cliente>();
+
+            XmlDocument docXml = new XmlDocument();
+            docXml.Load(etiquetFichero);
+
+            if (docXml != null)
+            {
+                foreach (XmlNode clientes in docXml.DocumentElement.ChildNodes)
+                {
+                    string dni = "";
+                    string nombre = "";
+                    string telefono = "";
+                    string email = "";
+                    string direccionPostal = "";
+
+                    foreach (XmlNode cliente in clientes.ChildNodes)
+                    {
+
+                        switch (cliente.Name)
+                        {
+                            case "dni":
+                                dni = cliente.InnerText;
+                                break;
+
+                            case "nombre":
+                                nombre = cliente.InnerText;
+                                break;
+
+                            case "telefono":
+                                telefono = cliente.InnerText;
+                                break;
+
+                            case "email":
+                                email = cliente.InnerText;
+                                break;
+
+                            case "direccionPostal":
+                                direccionPostal = cliente.InnerText;
+                                break;
+                        }
+
+                    }
+
+                    Cliente c = new Cliente(dni, nombre, telefono, email, direccionPostal);
+                    toret.Add(c);
+                }
+            }
+
+            return toret;
+        }
+
+        public static void GuardarClientes(string etiquetFichero, List<Cliente> clientes)
+        {
+            using (var xmlWriter = new XmlTextWriter(etiquetFichero, Encoding.UTF8))
+            {
+
+                xmlWriter.WriteStartElement("clientes");
+
+                foreach (var cliente in clientes)
+                {
+                    xmlWriter.WriteStartElement("cliente");
+
+
+                    xmlWriter.WriteStartElement("dni");
+                    xmlWriter.WriteString(cliente.Dni);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("nombre");
+                    xmlWriter.WriteString(cliente.Nombre);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("telefono");
+                    xmlWriter.WriteString(cliente.Telefono);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("email");
+                    xmlWriter.WriteString(cliente.Email);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteStartElement("direccionPostal");
+                    xmlWriter.WriteString(cliente.DireccionPostal);
+                    xmlWriter.WriteEndElement();
+
+
+                    xmlWriter.WriteEndElement();
+                }
+
+                xmlWriter.WriteEndElement();
+            }
+        }
     }
 }
--- App Gestion Bancaria/Core/Clases/Cliente.cs
+++ App Gestion Bancaria/Core/Clases/Cliente.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace App_Gestion_Bancaria.Core.Clases
 {
     public class Cliente
     {
+        public string Dni { get; private set; }
+        
+        public string Nombre { get; private set; }
+
+        public string Telefono { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string DireccionPostal { get; private set; }
+
+        public Cliente(string dni, string nombre, string telefono, string email, string direccionPostal)
+        {
+            this.Dni = dni;
+            this.Nombre = nombre;
+            this.Telefono = telefono;
+            this.Email = email;
+            this.DireccionPostal = direccionPostal;
+        }
+
+        public XElement ToXml()
+        {
+            XElement toret = new XElement("cliente", 
+                new XElement("dni", this.Dni),
+                new XElement("nombre", this.Nombre),
+                new XElement("telefono", this.Telefono),
+                new XElement("email", this.Email),
+                new XElement("direccionPostal", this.DireccionPostal)
+                );
+
+            return toret;
+        }
+
     }
 }
