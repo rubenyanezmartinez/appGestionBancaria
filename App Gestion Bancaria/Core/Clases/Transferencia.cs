8171c7f20ad4045f9f1b7f7a9e841d5d7d95ce8d
janasm <jsmartin@esei.uvigo.es>
2019-11-20 12:31:35 +0100

    Cambios modelo e interfaz de transferencias

--- App Gestion Bancaria/Core/Clases/Transferencia.cs
+++ App Gestion Bancaria/Core/Clases/Transferencia.cs
@@ -11,12 +11,26 @@ namespace App_Gestion_Bancaria.Core.Clases
     {
         public int Id { get; private set; }
         public String Tipo { get; private set; }
-        public Cuenta CCCOrigen { get; private set; }
-        public Cuenta CCCDestino { get; private set; }
+        //public Cuenta CCCOrigen { get; private set; }
+        public String CCCOrigen { get; private set; }
+
+        //public Cuenta CCCDestino { get; private set; }
+        public String CCCDestino { get; private set; }
         public double Importe { get; private set; }
         public DateTime Fecha { get; set; }
 
+        /*
         public Transferencia (int id, String tipo, Cuenta CCCOrigen, Cuenta CCCDestino, double importe, DateTime fecha)
+        {
+            this.Id = id;
+            this.Tipo = tipo;
+            this.CCCOrigen = CCCOrigen;
+            this.CCCDestino = CCCDestino;
+            this.Importe = importe;
+            this.Fecha = fecha;
+        }*/
+
+        public Transferencia(int id, String tipo, String CCCOrigen, String CCCDestino, double importe, DateTime fecha)
         {
             this.Id = id;
             this.Tipo = tipo;
@@ -32,8 +46,10 @@ namespace App_Gestion_Bancaria.Core.Clases
             XElement toret = new XElement("transferencia",
                 new XElement("id", this.Id.ToString()),
                 new XElement("tipo", this.Tipo.ToString()),
-                new XElement("cccorigen", this.CCCOrigen.ToXML()),
-                new XElement("cccdestino", this.CCCDestino.ToXML()),
+                //new XElement("cccorigen", this.CCCOrigen.ToXML()),
+                new XElement("cccorigen", this.CCCOrigen.ToString()),
+                //new XElement("cccdestino", this.CCCDestino.ToXML()),
+                new XElement("cccdestino", this.CCCDestino.ToString()),
                 new XElement("importe", this.Importe.ToString()),
                 new XElement("Fecha", this.Fecha.ToString())
                 );
