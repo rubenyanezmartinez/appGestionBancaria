24ae1dce1c178c590bad0bd3ec8676746f878850
janasm <jsmartin@esei.uvigo.es>
2019-11-26 20:03:30 +0100

    no message

--- Proyectos/Program.cs
+++ Proyectos/Program.cs
@@ -18,17 +18,12 @@ namespace Proyectos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ClienteController().View);
+            //Application.Run(new ClienteController().View);
             //Application.Run(new ProductosPersonaController().View);
             //GestorTransferencias gestorTransferencias = new GestorTransferencias();
             //GestorCuentas gestorCuentas = new GestorCuentas();
             //Application.Run(new TransferenciaController(gestorTransferencias, gestorCuentas).View);
-<<<<<<< HEAD
             //Application.Run(new CuentaController().View);
-            //Application.Run(new ClienteController().View);
-=======
-
->>>>>>> 5141d03ba43d00cb6614dcf13235c6abfe2585b1
         }
     }
 }
